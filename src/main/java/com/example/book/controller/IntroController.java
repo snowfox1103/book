62a0378f8c91841fffc74bfe0bfdbfda675fa493
snowfox <231829930+snowfox1103@users.noreply.github.com>
2025-10-01 11:57:05 +0900

    1001 석준영 - 0920

---
+++ src/main/java/com/example/book/controller/IntroController.java
@@ -0,0 +1,18 @@
+package com.example.book.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class IntroController {
+    @GetMapping
+    public String mainPage() {
+        return "mainPage/intro";
+    }
+}
